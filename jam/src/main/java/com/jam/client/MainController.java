58b74119f59c31d3c151f5410ce57114efab5df6
Lee Jinri <ar971004@naver.com>
2023-07-27 02:19:01 +0900

    메인 페이지 :  게시판 글 리스트 추가

--- jam/src/main/java/com/jam/client/MainController.java
+++ jam/src/main/java/com/jam/client/MainController.java
@@ -1,20 +1,38 @@
 package com.jam.client;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.jam.client.community.vo.CommunityVO;
+import com.jam.client.fleaMarket.vo.FleaMarketVO;
+import com.jam.client.job.vo.JobVO;
+import com.jam.client.roomRental.vo.RoomRentalVO;
+import com.jam.client.service.MainService;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private MainService mainService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home() {
+	public String home(Model model) {
 		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("main");
+		List<JobVO> jobList = mainService.jobList();
+		List<RoomRentalVO> roomList = mainService.roomList();
+		List<FleaMarketVO> fleaList = mainService.fleaList();
+		List<CommunityVO> comList = mainService.comList();
 		
-		return mav;
+		model.addAttribute("jobList", jobList);
+		model.addAttribute("roomList", roomList);
+		model.addAttribute("fleaList", fleaList);
+		model.addAttribute("comList", comList);
+		
+		return "main";
 	}
 }
