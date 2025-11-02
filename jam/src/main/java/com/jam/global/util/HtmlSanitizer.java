e54728696a8ce44394bb5ecd6461a4f1c0693f90
Lee Jinri <ar971004@naver.com>
2025-11-03 03:46:55 +0900

    feat(util): HtmlSanitizer 유틸 추가
    - 신뢰하지 않는 사용자 입력의 HTML 태그 검출 유틸 제공
    - 회원가입, 게시글/댓글 작성, 프로필 수정 등 전역 입력 검증에 활용

---
+++ jam/src/main/java/com/jam/global/util/HtmlSanitizer.java
@@ -0,0 +1,19 @@
+package com.jam.global.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
+public final class HtmlSanitizer {
+	private HtmlSanitizer() {}
+	
+	/**
+	 * 입력 문자열에 HTML 태그가 포함되어 있는지 검사합니다.
+	 *
+	 * @param input 검사할 문자열
+	 * @return true - HTML 태그가 포함되어 있음 / false - 순수 텍스트
+	 */
+	public static boolean hasHtmlTag(String input) {
+		String cleaned = Jsoup.clean(input, Safelist.none());
+		return !cleaned.equals(input);
+	}
+}
