d63529665c77824a4e3a29b48036697291667db7
Lee Jinri <ar971004@naver.com>
2025-12-14 09:38:27 +0900

    chore: WebInitializer public 클래스명 파일명과 일치

--- jam/src/main/java/com/jam/config/WebInitializer.java
+++ jam/src/main/java/com/jam/config/WebInitializer.java
@@ -3,19 +3,13 @@ package com.jam.config;
 import javax.servlet.Filter;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.jam.client.chat.webSocket.WebSocketConfig;
 import com.jam.global.redis.RedisConfig;
 
-
-//@Configuration
-//@ComponentScan(basePackages = {"com.jam"})
-//@PropertySource("classpath:application.properties")
-public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
     
-	
 	@Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{RootConfig.class, SecurityConfig.class, WebSocketConfig.class, RedisConfig.class}; 
