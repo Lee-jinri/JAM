b5856134b5ace255a86143d02b7332cc9defe0d3
Lee Jinri <ar971004@naver.com>
2026-03-04 04:25:20 +0900

    feat(security): CSRF 쿠키 발급을 위한 CsrfCookieFilter 구현체 추가
    - OncePerRequestFilter를 상속받아 매 요청 시 CSRF 토큰을 확인하고 쿠키를 갱신/유지하는 필터 클래스 추가

---
+++ src/main/java/com/jam/global/security/CsrfCookieFilter.java
@@ -0,0 +1,25 @@
+package com.jam.global.security;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CsrfCookieFilter extends OncePerRequestFilter{
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrfToken != null) {
+            csrfToken.getToken();
+        }
+        filterChain.doFilter(request, response);
+	}
+	
+}
