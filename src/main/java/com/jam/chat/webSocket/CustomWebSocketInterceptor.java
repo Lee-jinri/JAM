359eaa0953bef4336fc49995f608aca722f6af0a
Lee Jinri <ar971004@naver.com>
2026-03-05 02:32:32 +0900

    chore(chat): 웹소켓 기본 인프라 및 설정 파일 추가
    - pom.xml: spring-boot-starter-websocket 의존성 추가
    - WebSocketConfig: WebSocketConfigurer 구현 및 /ws 엔드포인트 등록
    - WebSocketHandler & Interceptor: 저수준(Native) 웹소켓 핸들러 및 인터셉터 복구

---
+++ src/main/java/com/jam/chat/webSocket/CustomWebSocketInterceptor.java
@@ -0,0 +1,73 @@
+package com.jam.chat.webSocket;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import com.jam.global.jwt.JwtTokenProvider;
+import com.jam.global.jwt.TokenInfo.TokenStatus;
+import com.jam.member.dto.MemberDto;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class CustomWebSocketInterceptor implements HandshakeInterceptor{
+	
+	private final JwtTokenProvider jwtTokenProvider;
+
+	public CustomWebSocketInterceptor(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+	
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+		HttpServletRequest httpRequest = servletRequest.getServletRequest();
+		
+		Cookie[] cookies = httpRequest.getCookies();
+		String accessToken = null;
+
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				if ("Authorization".equals(c.getName())) {
+					accessToken = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		// 토큰 없거나 유효하지 않으면 연결 거부
+		if (accessToken == null ||
+			jwtTokenProvider.validateToken(accessToken) != TokenStatus.VALID) {
+			return false;
+		}
+		
+		Authentication auth = jwtTokenProvider.getAuthentication(accessToken);
+		if(auth == null) return false;
+		
+		MemberDto user = (MemberDto) auth.getPrincipal();
+		
+		attributes.put("userId", user.getUser_id());
+		attributes.put("roles", user.getRoles());
+		
+		return true;
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception exception) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
