5ec9dee95afaecdc191683356cd7d1c4089eb3fb
Lee Jinri <ar971004@naver.com>
2026-07-19 21:19:25 +0900

    fix(FileAsset): 이미지 다중 업로드 시 S3 자격증명 초기화 레이스 컨디션 수정
    - S3Client/S3Presigner가 각자 DefaultCredentialsProvider를 따로 생성하던 것을 단일 빈으로 공유하도록 변경

--- src/main/java/com/jam/config/S3PresignerConfig.java
+++ src/main/java/com/jam/config/S3PresignerConfig.java
@@ -1,8 +1,10 @@
 package com.jam.config;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -11,19 +13,31 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3PresignerConfig {
 	
+	@Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return DefaultCredentialsProvider.builder().build();
+    }
+	
 	@Bean(destroyMethod = "close")
-	public S3Client s3Client() {
+	public S3Client s3Client(AwsCredentialsProvider awsCredentialsProvider) {
 		return S3Client.builder()
 			.region(Region.AP_NORTHEAST_2)
-			.credentialsProvider(DefaultCredentialsProvider.builder().build())
+			.credentialsProvider(awsCredentialsProvider)
 			.build();
 	}
 	
 	@Bean(destroyMethod = "close") 
-    public S3Presigner s3Presigner() {
+    public S3Presigner s3Presigner(AwsCredentialsProvider awsCredentialsProvider) {
 		 return S3Presigner.builder()
 				 .region(Region.AP_NORTHEAST_2) // 서울
-				 .credentialsProvider(DefaultCredentialsProvider.builder().build())
+				 .credentialsProvider(awsCredentialsProvider)
 				 .build();
+    }
+	
+	@Bean
+    public ApplicationRunner warmUpAwsCredentials(AwsCredentialsProvider awsCredentialsProvider) {
+        return args -> {
+            awsCredentialsProvider.resolveCredentials();
+        };
     }
 }
