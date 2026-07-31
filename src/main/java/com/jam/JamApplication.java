500a736b6f6c390698b41affb8fdb9188fcb2258
Lee Jinri <ar971004@naver.com>
2026-08-01 04:22:11 +0900

    refactor: @MapperScan을 MyBatisConfig로 분리
    - JamApplication에 있던 @MapperScan을 별도 @Configuration 클래스로 이동.
    - @WebMvcTest 슬라이스 테스트에서 MyBatisConfig를 excludeFilters로 제외할 수 있게 하기 위함.
    - 실제 앱 구동 시에는 컴포넌트 스캔 범위 안에 그대로 포함되어 동작에 영향 없음.

--- src/main/java/com/jam/JamApplication.java
+++ src/main/java/com/jam/JamApplication.java
@@ -1,10 +1,8 @@
 package com.jam;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.jam.**.mapper")
 @SpringBootApplication
 public class JamApplication {
 
