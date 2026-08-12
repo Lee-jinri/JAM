901e3d53c78b22d9fd0aa3a1be813236ac5c012a
Lee Jinri <ar971004@naver.com>
2026-08-13 08:19:43 +0900

    fix(File): 주석 처리되어 있던 파일 다운로드 권한 검사 주석 해제
    - FileAccessService.existsFileAccess의 권한 판단 로직 (if(access == 0) throw ForbiddenException)이 주석 처리 되어 있어 항상 true를 반환하는 문제가 있어 주석을 해제하여 해결함.

--- src/main/java/com/jam/global/service/FileAccessService.java
+++ src/main/java/com/jam/global/service/FileAccessService.java
@@ -2,6 +2,7 @@ package com.jam.global.service;
 
 import org.springframework.stereotype.Service;
 
+import com.jam.global.exception.ForbiddenException;
 import com.jam.global.mapper.FileAccessMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -10,12 +11,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FileAccessService {
 	private final FileAccessMapper fileAccessMapper;
-	
+
 	public boolean existsFileAccess(String userId, Long fileId) {
-		
+
 		int access = fileAccessMapper.existsFileAccess(userId, fileId);
-	    //if(access == 0) throw new ForbiddenException("다운로드 할 권한이 없습니다.");
-		
+	    if(access == 0) throw new ForbiddenException("다운로드 할 권한이 없습니다.");
+
 		return true;
 	}
 }
